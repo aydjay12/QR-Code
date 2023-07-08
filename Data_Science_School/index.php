a64d8f0144a007ec677901af28a76fb79b985070
olalekan github <aydjay.gmail.net>
2023-07-08 20:35:49 +0100

    i added

---
+++ Data_Science_School/index.php
@@ -0,0 +1,237 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DATA SCIENCE SCHOOL</title>
+    <link rel="stylesheet" href="Assets/font.css">
+    <link rel="stylesheet" href="Assets/data_science_school.css">
+    <link href="https://fonts.googleapis.com/css2?family=Raleway+Dots&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/e0930b8f2a.js"></script>
+</head>
+<body>
+    <main>
+        <section class="main_container">
+            <section class="home">
+                <section class="header">
+                    <section class="image">
+                        <img src="Assets/Images/Picture1.png" alt="">
+                        <img src="Assets/Images/Picture2.png" alt="">
+                    </section>
+                    <section class="nav_bar">
+                        <img src="Assets/Images/1200px-Hamburger_icon_white.svg.png" alt="">
+                        <ul>
+                            <li><a href="#">Speakers</a></li>
+                            <li><a href="#">Accomodation</a></li>
+                            <li><a href="#">Sponsors</a></li>
+                            <li><a href="#">Register</a></li>
+                        </ul>
+                    </section>
+                </section>
+                <section class="showcase">
+                    <section class="sub_topic">
+                        <p>Department of Statistics, University of Lagos In Conjuction with Statistics Without Borders Presents</p>
+                    </section>
+                    <section class="topic">
+                        <p>
+                            DATA SCIENCE SCHOOL: 
+                        </p>
+                        <p>
+                            Industry Engagement And Applications
+                        </p>
+                    </section>
+                    <section class="related_topics">
+                        <p>Some Related Topics To Be Treated</p>
+                        <section>
+                            <ul>
+                               <li>Deep Learning</li>
+                               <li>Machine Learning</li>
+                               <li>Data Mining</li>
+                               <li>Big Data Analytics</li>
+                               <li>Artificial Intelligence</li>
+                               <li>Data Visualization</li>
+                               <li>DataBase (Including SQL)</li>
+                               <li>Ethics</li> 
+                            </ul>
+                        </section>
+                    </section>
+                    <section class="date_time">
+                        <p><i class="fa-solid fa-location-dot" style="color: #87cefa;"></i>University Of Lagos</p>
+                        <p><i class="fa-regular fa-calendar-days" style="color: #87cefa;"></i>7<sup>th</sup> - 18<sup>th</sup>August, 2023</p>
+                        <p><i class="fa-regular fa-clock" style="color: #87cefa;"></i>9am - 5pm daily</p>
+                    </section>
+                </section>
+            </section>
+            <section class="Who_To_Attend_And_Requirement">
+                <section class="Who_to_Attend">
+                    <p>Who To Attend</p>
+                    <section>
+                        <p><i class="fa-solid fa-arrow-right" style="color: #87cefa;"></i>Educators</p>
+                        <p><i class="fa-solid fa-arrow-right" style="color: #87cefa;"></i>Goverment And Industry Personnel</p>
+                        <p><i class="fa-solid fa-arrow-right" style="color: #87cefa;"></i>Students</p>
+                        <p><i class="fa-solid fa-arrow-right" style="color: #87cefa;"></i>Other Interested Persons</p>
+                    </section>
+                </section>
+                <section class="Requirement">
+                    <p>Participants Requirement</p>
+                    <section>
+                        <p><i class="fa-solid fa-arrow-right" style="color: #87cefa;"></i>A fair Knowledge on the use of Python programming</p>
+                        <p><i class="fa-solid fa-arrow-right" style="color: #87cefa;"></i>A Personal laptop for practical sessions</p>
+                        <p><i class="fa-solid fa-arrow-right" style="color: #87cefa;"></i>Likely use of Google Collaborator</p>
+                    </section>
+                </section>
+            </section>
+            <section class="Speakers" id="Speakers">
+                <div class="top">
+                    <h3>SPEAKERS</h3>
+                    <div>
+                        <h1>MEET THE INCREDIBLE SPEAKERS</h1>
+                        <h2>7-18 <br>AUG</h2>
+                    </div>
+                </div>
+                <div class="bottom-3">
+                    <div>
+                        <img src="\Data_Science_School\Assets\Images\Chief-Host-Picture.png">
+                        <div>
+                            <h6 style="font-weight: 400;">Professor Folashade Ogunshola</h6>
+                            <h4>Chief Host</h4>
+                            <p>Vice Chancellor</p>
+                        </div>
+                    </div>
+                    <div>
+                        <img src="\Data_Science_School\Assets\Images\Host-Picture.png">
+                        <div>
+                            <h6 style="font-weight: 400;">Professor Elijah Oyeyemi</h6>
+                            <h4>Host</h4>
+                            <p>Dean, Faculty of Science</p>
+                        </div>
+                    </div>
+                    <div>
+                        <img src="\Data_Science_School\Assets\Images\Co-Host-Picture.png">
+                        <div>
+                            <h6 style="font-weight: 400;">Professor Muminu Adamu</h6>
+                            <h4>Co-Host</h4>
+                            <p>HOD, Department of Statistics</p>
+                        </div>
+                    </div>
+                    <div>
+                        <img src="/Data_Science_School\Assets\Images\Speaker-Picture-1.png">
+                        <div>
+                            <h6 style="font-weight: 400;">Associate Professor Tapasi Ghosh</h6>
+                            <h4>Department of Data Sciences and Analytics</h4>
+                            <p>M.S.Ramaiah University of Applied Sciences, India</p>
+                        </div>
+                    </div>
+                    <div>
+                        <img src="/Data_Science_School\Assets\Images\Speaker-Picture-2.png">
+                        <div>
+                            <h6 style="font-weight: 400;">Rose M. Ray, Ph.D.</h6>
+                            <h4>Principal Scientist</h4>
+                            <p>Data Sciences (Exponent)</p>
+                        </div>
+                    </div>
+                    <div>
+                        <img src="/Data_Science_School\Assets\Images\Speaker-Picture-3.png">
+                        <div>
+                            <h6 style="font-weight: 400;">Luca Alberto Rizo</h6>
+                            <h4>Senior Research Data Scientist</h4>
+                            <p>Huewei Ireland Research Center</p>
+                        </div>
+                    </div>
+                    <div>
+                        <img src="/Data_Science_School\Assets\Images\Speaker-Picture-4.png">
+                        <div>
+                            <h6 style="font-weight: 400;">Aleksander Da Silva Boski</h6>
+                            <h4>Data Analytics Teacher</h4>
+                            <p>IronHack</p>
+                        </div>
+                    </div>
+                    <div>
+                        <img src="/Data_Science_School\Assets\Images\Speaker-Picture-5.png">
+                        <div>
+                            <h6 style="font-weight: 400;">Professor E. Olubusoye</h6>
+                            <p>University of Ibadan</p>
+                        </div>
+                    </div>
+                    <div>
+                        <img src="/Data_Science_School\Assets\Images\Speaker-Picture-6.png">
+                        <div>
+                            <h6 style="font-weight: 400;">Dr. Ayodeji Akiwowo</h6>
+                            <h4>Senior Data Scientist</h4>
+                            <p>FUJITSU, UK</p>
+                        </div>
+                    </div>
+                    <div>
+                        <img src="/Data_Science_School\Assets\Images\Speaker-Picture-7.png">
+                        <div>
+                            <h6 style="font-weight: 400;">Associate Professor Sahana Prasad</h6>
+                            <h4>Applied Statistician & Operations Research</h4>
+                        </div>
+                    </div>
+                    <div>
+                        <img src="/Data_Science_School\Assets\Images\Speaker-Picture-8.png">
+                        <div>
+                            <h6 style="font-weight: 400;">Associate Professor Wijayatunga Priyantha</h6>
+                            <p>Umea University, Sweden</p>
+                        </div>
+                    </div>
+                    <div>
+                        <img src="/Data_Science_School\Assets\Images\Speaker-Picture-9.png">
+                        <div>
+                            <h6 style="font-weight: 400;">Professor Christopher Thron</h6>
+                            <p>Texas A & M University, USA</p>
+                        </div>
+                    </div>
+                </div>
+                <div id="button"><button>SEE ALL SPEAKERS</button></div>
+                <div id="others" class="others">
+                    <div class="top-2">
+                        <h2>Some Other Speakers:</h2>
+                    </div>
+                    <div class="bottom-2">
+                        <div class="online">
+                            <h3>Prof. Shalini S. Gopalkrishnan</h3>
+                            <p>Valencia College</p>
+                        </div>
+                        <div class="online">
+                            <h3>Junjun Huo</h3>
+                            <p>Senior Data Scientist, Shopify</p>
+                        </div>
+                        <div class="online">
+                            <h3>Nwachukwu Chibuzor Joshua</h3>
+                            <p>Data Scientist, Metis Analytics</p>
+                        </div>
+                        <div class="online">
+                            <h3>Oliver Hugh</h3>
+                            <p>Medical Statistician, Perinatal Institute, UK</p>
+                        </div>
+                        <div class="online">
+                            <h3>Janine Wicke</h3>
+                            <p>Data Scientist</p>
+                        </div>
+                        <div class="online">
+                            <h3>Parul Sehrawat</h3>
+                            <p>Data Scientist, UK</p>
+                        </div>
+                        <div class="online">
+                            <h3>Ernesto L. Garcia (PhD)</h3>
+                            <p>University of Missouri, Columbia</p>
+                        </div>
+                        <div class="online">
+                            <h3>Aiwin Aind</h3>
+                            <p>Data Scientist, Delhi Tech. University</p>
+                        </div>
+                    </div>
+                </div>
+                <button id="arrow"><img src="Assets/images/arrow-5.png"></button>
+            </section>
+            <section class="Register" id="Register"></section>
+            <section class="Accomodation" id="Accomodation"></section>
+            <section class="Sponsors" id="Sponsors"></section>
+        </section>
+    </main>
+</body>
+
+<script src="home.js"></script>
+</html>
